a1a5e8315a5f515a0710761f6a549b1cc233cc15
Aria Samandi <ariasamandi@gmail.com>
2024-04-24 15:29:14 -0700

    Initial Commit

---
+++ StatPadderSwiftUI/StatPadderSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatPadderSwiftUIApp.swift
+//  StatPadderSwiftUI
+//
+//  Created by patron on 4/24/24.
+//
+
+import SwiftUI
+
+@main
+struct StatPadderSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
