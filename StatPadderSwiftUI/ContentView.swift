a6b895f32da8ec43b73813d12f5bfed0ca0109d9
Aria Samandi <ariasamandi@gmail.com>
2024-04-24 15:46:40 -0700

    First commit: Created a title and start button to start the app off

--- StatPadderSwiftUI/ContentView.swift
+++ StatPadderSwiftUI/ContentView.swift
@@ -10,10 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "book.closed.circle")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Welcome to StatPadder")
+            Button("Start Here") {
+                
+            }.padding(.top, 7.0).buttonStyle(.borderedProminent).tint(.green)
         }
         .padding()
     }
